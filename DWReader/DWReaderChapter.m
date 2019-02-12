1eeeb895eaa21ebdc134bba20eb1fbbf1f34ffad
Wicky <codewicky@163.com>
2019-02-13 02:25:13 +0800

    分段代码改进，并添加必要注释

--- DWReader/DWReaderChapter.m
+++ DWReader/DWReaderChapter.m
@@ -59,19 +59,18 @@
         [content replaceCharactersInRange:NSMakeRange(content.length - kSeperateParagraphLength, kSeperateParagraphLength) withString:@""];
     }
     
-    ///匹配段落
-    NSArray <NSTextCheckingResult *>* results = [[[NSRegularExpression alloc] initWithPattern:kSeperateParagraphString options:0 error:nil] matchesInString:content options:0 range:NSMakeRange(0, content.length)];
-    
     ///获取段落以后处理段首缩进及段落间距，由于之前去除了段首的分段符，所以现在首先应该给段首添加缩进
     [content insertString:kIndentString atIndex:0];
+    
+    ///匹配段落
+    NSArray <NSTextCheckingResult *>* results = [[[NSRegularExpression alloc] initWithPattern:kSeperateParagraphString options:0 error:nil] matchesInString:content options:0 range:NSMakeRange(0, content.length)];
     
     ///然后计算段落信息
     NSUInteger resultsCnt = results.count;
     NSMutableArray * paraTmp = [NSMutableArray arrayWithCapacity:resultsCnt + 1];
     __block NSUInteger lastLoc = 0;
     [results enumerateObjectsUsingBlock:^(NSTextCheckingResult * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        ///计算段落信息，其中LastLoc表示计算本段落的起始位置，除了首次采取默认值0以外，其他均为seperateParagraph方法返回的位置，该方法返回的位置是包括给段首添加
-        lastLoc = [self seperateParagraphWithString:content paras:paraTmp lastLoc:lastLoc nextLoc:obj.range.location + kIndentLength];
+        lastLoc = [self seperateParagraphWithString:content paras:paraTmp lastLoc:lastLoc nextLoc:obj.range.location];
     }];
     
     ///补充最后一段
@@ -82,15 +81,19 @@
     
     NSLog(@"%@",paraTmp);
     NSLog(@"\n%@",self.parsedString);
+    
+    ///至此完成分段，后续应该添加空白字符
 }
 
 #pragma mark --- tool method ---
 -(NSUInteger)seperateParagraphWithString:(NSMutableString *)str paras:(NSMutableArray *)paras lastLoc:(NSUInteger)lastLoc nextLoc:(NSUInteger)nextLoc {
     
+    ///计算段落信息，其中LastLoc表示计算本段落的起始位置，nextLoc表示结束位置。起始位置除手段从默认值0开始计算以外，其他均为上一段落结束位置后加一个结尾换行符的长度的位置。结束位置及每次匹配到的分段符的Location。（语言表述能力有限，实在想不明白建议画个图）
     DWReaderParagraph * para = [DWReaderParagraph new];
     para.range = NSMakeRange(lastLoc,nextLoc - lastLoc);
     [paras addObject:para];
     
+    ///之所以要加一个结尾换行符长度是因为在结尾换行符后我们后续会添加空白字符来调整段落间距，事实上我们分段就是为了找这个位置及段首缩进。所以找到这个位置很重要
     return para.range.location + para.range.length + kFooterLineBreakLength;
 }
 
